7644e327f340f95db6f99878ef144a64332c18a1
Dewald <dewaldfourie0808@gmail.com>
2024-03-27 19:02:05 +0200

    added bcrypt for password handling and comments

--- app.js
+++ app.js
@@ -6,15 +6,16 @@ const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const { log } = require("console");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
 
 
-
+// get data base through URL and connect to it
 const mongoDb = "mongodb+srv://dewaldfourie08:WtWfgU!RH5J8bcN@cluster0.ewk5etm.mongodb.net/passport_authentication?retryWrites=true&w=majority";
 mongoose.connect(mongoDb);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
 
-
+// create a new Schema Model for a user (This should ideally be in a separate file)
 const User = mongoose.model(
     "User",
     new Schema({
@@ -23,32 +24,44 @@ const User = mongoose.model(
     })
 );
 
+// setting app express and view engine PUG
 const app = express();
 app.set("views", __dirname);
 app.set("view engine", "pug");
-
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
+// creating a local variable for usage in views 
 app.use((req, res, next) => {
     res.locals.currentUser = req.user;
     next();
 });
 
+// main get route
 app.get("/", (req, res) => {
     res.render("index", { user: req.user });
 });
 
+// sign up routes
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 app.post("/sign-up", async(req, res, next) => {
     try {
-        const user = new User({
-            username: req.body.username,
-            password: req.body.password,
+        // hash function for password salting
+        bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+            // if hash error > return next err
+            if (err) {
+                return next (err)
+            }
+            // else create a new user with username and hashed password 
+            const user = new User({
+                username: req.body.username,
+                password: hashedPassword,
+            });
+            // save new user to db
+            await user.save();
+            res.redirect("/");
         });
-        const result = await user.save();
-        res.redirect("/");
     } catch(err) {
         return next(err);
     };
@@ -56,6 +69,7 @@ app.post("/sign-up", async(req, res, next) => {
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
 
+// using passport with the LocalStrategy to handle login functionality
 passport.use(
     new LocalStrategy(async (username, password, done) => {
         try {
@@ -63,7 +77,8 @@ passport.use(
             if (!user) {
                 return done(null, false, { message: "Incorrect Username" });
             }
-            if (user.password !== password) {
+            const match = await bcrypt.compare(password, user.password)
+            if (!match) {
                 return done(null, false, { message: "Incorrect Password" });
             };
             return done(null, user);
@@ -86,11 +101,13 @@ passport.deserializeUser(async (id, done) => {
     };
 });
 
+// login route en redirect options
 app.post("/log-in", passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/",
 }))
 
+// logout route and redirect options
 app.get("/log-out", (req, res, next) => {
     req.logout((err) => {
         if(err) {
